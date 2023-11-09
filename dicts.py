b891782f448ad5f8ff37985c0763b64651d3906f
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2023-11-09 04:22:50 +0000

    Added doctest invert({})

--- dicts.py
+++ dicts.py
@@ -3,8 +3,10 @@ def invert(d: dict) -> dict:
 
     If the dict is not injective an arbitrary choice is made.
 
-    >>> invert({"one":1, "pi":3, "tau":6.283185307179586})
+    >>> invert({"one": 1, "pi": 3, "tau": 6.283185307179586})
     {1: 'one', 3: 'pi', 6.283185307179586: 'tau'}
+    >>> invert({})
+    {}
     """
     inv_d = {}
     for elm in d:

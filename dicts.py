2a3465a9f1457f8d60ab23deae704426d60a41ad
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2023-11-11 02:45:13 +0000

    alternate implemantation of adjacency()

--- dicts.py
+++ dicts.py
@@ -33,8 +33,7 @@ def adjacency(edges: list[tuple[str,str]]) -> dict[str,set[str]]:
     """
     adj_list = {}
     for source, dest in edges:
-        if source in adj_list:
-            adj_list[source].add(dest)
-        else:
-            adj_list[source] = {dest}
+        if source not in adj_list:
+            adj_list[source] = set()
+        adj_list[source].add(dest)
     return adj_list

ed0c95e7b4094c195bf5eaad91064713d6614913
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2024-01-04 07:53:42 +0000

    detect OSError when opening or reading the file

--- numlines.py
+++ numlines.py
@@ -8,12 +8,19 @@ import sys
 def main():
     """Open file and display numbered lines."""
     if len(sys.argv) != 2:
-        print("Usage: numlines.py filename", file=sys.stderr)
-        sys.exit(2)
+        _die(f"Usage: {sys.argv[0]} filename", 2)
 
-    with open(sys.argv[1], "r", encoding="utf-8") as file:
-        for i, line in enumerate(file):
-            print(f"{i}: {line}", end="")
+    try:
+        with open(sys.argv[1], "r", encoding="utf-8") as file:
+            for i, line in enumerate(file):
+                print(f"{i}: {line}", end="")
+    except OSError as err:
+        _die(f"{sys.argv[0]}: {err}", 1)
+
+
+def _die(message, error):
+    print(message, file=sys.stderr)
+    sys.exit(error)
 
 
 if __name__ == "__main__":

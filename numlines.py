44c972c66b5300c401d86b883b7dba3d0c984c84
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2024-01-04 07:40:27 +0000

    open file with with

--- numlines.py
+++ numlines.py
@@ -11,14 +11,11 @@ def main():
         print("Usage: numlines.py filename", file=sys.stderr)
         sys.exit(2)
 
-    file = open(sys.argv[1], "r", encoding="utf-8")
-    try:
+    with open(sys.argv[1], "r", encoding="utf-8") as file:
         i = 0
         for line in file:
             print(f"{i}: {line}", end="")
             i += 1
-    finally:
-        file.close()
 
 
 if __name__ == "__main__":

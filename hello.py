72d80e88cc81294d4d3b4503ccf7f08559e0c61d
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2024-01-04 06:37:17 +0000

    used a lookup table and added a doctest to hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+
 """Hello World Program Hello =D"""  # noqa: D400 D415
+
+_GREETINGS = ["hel-load world", "hello world"]
 
 
 def main(*, script=False):
-    """Print a friendly greeting."""
-    print("hello world" if script else "hel-load world")
+    """
+    Print a friendly greeting.
+
+    >>> main(script=False)
+    hel-load world
+
+    >>> main(script=True)
+    hello world
+    """
+    print(_GREETINGS[script])
 
 
 main(script= __name__ == "__main__")

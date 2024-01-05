5229a219975fac18e34b0d589ebb207168703e8d
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2024-01-05 00:37:14 +0000

    Modified greetall to remove superfluosu functions and remove default values

--- greetall.py
+++ greetall.py
@@ -4,40 +4,25 @@
 
 import sys
 
-from greet import greet
 from supply import distinct_gen
 
 
-def reader():
-    while True:
-        try:
-            line = input().strip()
-        except EOFError:
-            return
-        yield line
-
-
-def greet(name, *, file=sys.stdout):
-    """Greet by name."""
-    if name:
-        print(f"Hello, {name}!", file=file)
-
-
-def greet_all(names_file=sys.stdin, greet_file=sys.stdout):
+def greet_all(names_file, greet_file):
     for name in distinct_gen(map(str.strip, names_file)):
-        greet(name, file=greet_file)
+        if name:
+            print(f"Hello, {name}!", file=greet_file)
 
 
 def main():
     if len(sys.argv) > 3:
         _die(f"{sys.argv[0]}: error: too many arguments", 2)
     if len(sys.argv) == 1:
-        greet_all()
+        greet_all(sys.stdin, sys.stdout)
     else:
         try:
             with open(sys.argv[1], "r", encoding="utf-8") as file:
                 if len(sys.argv) == 2:
-                    greet_all(file)
+                    greet_all(file, sys.stdout)
                 else:
                     with open(sys.argv[2], "a", encoding="utf-8") as greetings:
                         greet_all(file, greetings)

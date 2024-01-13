2a875bee1196498e223853bcb7a98d90fab87938
tfallon0 <62510089+tfallon0@users.noreply.github.com>
2024-01-13 05:06:48 +0000

    to greetall, added docstrings and better variable name

--- greetall.py
+++ greetall.py
@@ -2,29 +2,31 @@
 
 """Greet all script."""
 
-from contextlib import nullcontext, ExitStack
 import sys
+from contextlib import ExitStack
 
 from supply import distinct_gen
 
 
 def greet_all(names_file, greet_file):
+    """Output a greeting to greet_file for each unique name that appears in names_file."""
     for name in distinct_gen(filter(None, map(str.strip, names_file))):
         print(f"Hello, {name}!", file=greet_file)
 
 
 def main():
+    """Run the program."""
     if len(sys.argv) > 3:
         _die(f"{sys.argv[0]}: error: too many arguments", 2)
-    file = sys.stdin
+    names = sys.stdin
     greetings = sys.stdout
     try:
         with ExitStack() as stack:
             if len(sys.argv) > 1:
-                file = stack.enter_context(open(sys.argv[1], "r", encoding="utf-8"))
+                names = stack.enter_context(open(sys.argv[1], "r", encoding="utf-8"))
                 if len(sys.argv) > 2:
                     greetings = stack.enter_context(open(sys.argv[2], "a", encoding="utf-8"))
-            greet_all(file, greetings)
+            greet_all(names, greetings)
     except OSError as err:
         _die(f"{sys.argv[0]}: error: {err}", 1)
 

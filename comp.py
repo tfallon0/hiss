a68b10eb8f00871d45ac84a92220a7e98a687f84
Eliah Kagan <degeneracypressure@gmail.com>
2024-01-14 18:54:03 +0000

    Add more comp.py exercises

--- comp.py
+++ comp.py
@@ -1,6 +1,7 @@
 """Basic iteration with loops, generators, and comprehensions."""
 
 import itertools
+from collections import defaultdict, deque
 from collections.abc import Iterable, Iterator
 
 
@@ -103,7 +104,9 @@ def list_squares_comp_alt(n: int) -> list[int]:
     This is an alternative implementation of list_squares_comp(). This
     implementation is also a single return statement, but it does not use the
     same kind of comprehension. Because list comprehensions exist, this is not
-    typically a reasonable technique.
+    typically a reasonable technique. There are two reasons to know about this
+    technique: so you can notice and avoid using it by accident, and because
+    some variations on it are both reasonable and useful.
 
     >>> list_squares_comp_alt(0)
     []
@@ -117,6 +120,26 @@ def list_squares_comp_alt(n: int) -> list[int]:
     []
     """
     return list(i**2 for i in range(1, n + 1))  # noqa: C400
+
+
+def tuple_squares_comp(n: int) -> tuple[int, ...]:
+    """
+    Make a tuple of squares from 1 to n, inclusive, using a comprehension.
+
+    The implementation of this function is a single return statement.
+
+    >>> tuple_squares_comp(0)
+    ()
+    >>> tuple_squares_comp(1)
+    (1,)
+    >>> tuple_squares_comp(10)
+    (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+    >>> tuple_squares_comp(14)
+    (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196)
+    >>> tuple_squares_comp(-1)
+    ()
+    """
+    return tuple(i**2 for i in range(1, n + 1))
 
 
 def generate_sevenless_loop(n: int) -> Iterator[int]:
@@ -161,7 +184,8 @@ def list_sevenless_squares_loop(n: int) -> list[int]:
     """
     Make a list of squares that have no factor of 7, using a loop.
 
-    This implementation does not call anything else in this project.
+    Like every other function in this module, this implementation does not call
+    anything else in this project, not even other functions in this module.
 
     >>> list_sevenless_squares_loop(16)
     [1, 4, 9, 16, 25, 36, 64, 81, 100, 121, 144, 169, 225, 256]
@@ -188,6 +212,21 @@ def list_sevenless_squares_comp(n: int) -> list[int]:
     [1, 4, 9, 16, 25, 36, 64, 81, 100, 121, 144, 169, 225, 256, 289]
     """
     return [i**2 for i in range(1, n + 1) if i % 7 != 0]
+
+
+def deque_sevenless_squares_comp(n: int) -> deque[int]:
+    """
+    Make a list of squares that have no factor of 7, using a comprehension.
+
+    The implementation of this function is a single return statement. It does
+    not create an intermediate list, nor call anything else in this project.
+
+    >>> deque_sevenless_squares_comp(16)
+    deque([1, 4, 9, 16, 25, 36, 64, 81, 100, 121, 144, 169, 225, 256])
+    >>> deque_sevenless_squares_comp(17)
+    deque([1, 4, 9, 16, 25, 36, 64, 81, 100, 121, 144, 169, 225, 256, 289])
+    """
+    return deque(i**2 for i in range(1, n + 1) if i % 7 != 0)
 
 
 def set_cubes_loop(n: int) -> set[int]:
@@ -254,6 +293,32 @@ def set_cubes_comp(n: int) -> set[int]:
     return {i**3 for i in range(1, n + 1)}
 
 
+def frozenset_cubes_comp(n: int) -> frozenset[int]:
+    """
+    Make a frozenset of cubes from 1 to n, inclusive, using a comprehension.
+
+    The implementation of this function is a single return statement. It does
+    not (at least not explicitly) create an intermediate set.
+
+    >>> frozenset_cubes_comp(0)
+    frozenset()
+    >>> frozenset_cubes_comp(1)
+    frozenset({1})
+    >>> expected = frozenset({1, 8, 27, 64, 125, 216, 343, 512, 729, 1000})
+    >>> actual = frozenset_cubes_comp(10)
+    >>> isinstance(actual, frozenset) and actual == expected
+    True
+    >>> expected = frozenset({1, 8, 27, 64, 125, 216, 343, 512, 729, 1000,
+    ...                       1331, 1728, 2197, 2744})
+    >>> actual = frozenset_cubes_comp(14)
+    >>> isinstance(actual, frozenset) and actual == expected
+    True
+    >>> frozenset_cubes_comp(-1)
+    frozenset()
+    """
+    return frozenset(i**3 for i in range(1, n + 1))
+
+
 def dict_words_loop(words: Iterable[str]) -> dict[str, int]:
     """
     Make a dictionary mapping words to their lengths using a loop.
@@ -302,12 +367,32 @@ def dict_words_comp_alt(words: Iterable[str]) -> dict[str, int]:
     same kind of comprehension. dict_words_comp() is how this actually should
     be done, while this alternative approach resembles dict_words_map(), though
     it does not make any use of the map builtin. Because dict comprehensions
-    exist, this is not typically a reasonable technique.
+    exist, this is not typically a reasonable technique. But it is useful to
+    know about, because some variations on it are reasonable and useful.
 
     >>> dict_words_comp_alt(['cat', 'ox', 'human', 'raven', 'crow'])
     {'cat': 3, 'ox': 2, 'human': 5, 'raven': 5, 'crow': 4}
     """
     return dict((word, len(word)) for word in words)  # noqa: C402
+
+
+def defaultdict_words_comp(words: Iterable[str]) -> defaultdict[str, int | None]:
+    """
+    Make a word-lengths defaultdict that associates None with unknown words.
+
+    The implementation of this function is a single return statement. Sometimes
+    it may be justifiable to create a dict first, because in some cases, that
+    may increase code readability. But this does not do that.
+
+    >>> d = defaultdict_words_comp(['cat', 'ox', 'human', 'raven', 'crow'])
+    >>> d  # doctest: +ELLIPSIS
+    defaultdict(..., {'cat': 3, 'ox': 2, 'human': 5, 'raven': 5, 'crow': 4})
+    >>> d['raven']
+    5
+    >>> d['eagle'] is None
+    True
+    """
+    return defaultdict(lambda: None, ((word, len(word)) for word in words))
 
 
 def matrix_indices_loop(m: int, n: int) -> Iterator[tuple[int, int]]:
